37ceb845014998ef7f0dabba6c2a34973d9eb8af
João Hideki <joaoohideki12345@gmail.com>
2024-09-26 00:24:27 -0300

    resolvendo conflitos

--- app/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { AngularMaterialModule } from "../angular-material/angular-material.module";
-
-@NgModule({
-  declarations: [],
-  imports: [AngularMaterialModule],
-  exports: []
-})
-export class ComponentsModule {}
