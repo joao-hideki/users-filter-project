82cd54f71097b6a58b63d1884a5d585b07d91cdc
Joao Hideki <joaoohideki12345@gmail.com>
2024-09-25 21:31:57 -0300

    adicionando @angular/material; ComponentsModule; AngularMaterialModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     ComponentsModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
