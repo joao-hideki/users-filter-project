382dc0f47818f69782049934c0dd71a1aca4f41d
João Hideki <joaoohideki12345@gmail.com>
2024-09-28 14:23:59 -0300

    binding UserDetailsComponent

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from 'src/app/interfaces/user/user.interface';
 
 @Component({
   selector: 'app-user-details',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent {
+  // formas de inicializar um objeto customizado
+
+  // @Input() user: IUser = {} as IUser; // fazer o casting e inciando um objeto vazio com os valores como undefined
+  // @Input() user: IUser = {/*inciando os valores das propriedades*/};
+  // @Input() user: Partial<IUser> = {}; // partial - significa que o objeto pode conter algumas propriedades da tipagem passada, mas não precisa seguir a risca
+  // @Input() user: IUser = new User(); // instanciar uma classe que implementa a interface
+  // class User implements IUser {
+  //   // inicializando os valores das propriedades
+  // }
+
+  @Input({required: true}) user: IUser = {} as IUser;
 
 }
+
+
