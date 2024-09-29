390c16dde0b75b40cb7667667abed1ea9b4c703e
João Hideki <joaoohideki12345@gmail.com>
2024-09-28 22:30:49 -0300

    capturando os valores dos filtros

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFilterOptions } from 'src/app/interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-filter',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
+  filterOptions: IFilterOptions = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined
+  };
 
+  statusList = [
+    {description: "Ativo", value: true},
+    {description: "Inativo", value: false}
+  ]
 }
