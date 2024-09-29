50698cee6fb3716f3672190b4ec2531c45e52ae7
João Hideki <joaoohideki12345@gmail.com>
2024-09-28 21:48:23 -0300

    exibindo detalhes do usuário ao clicá-lo na lista

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { UsersList } from 'src/app/data/users-list';
 import { IUser } from 'src/app/interfaces/user/user.interface';
 
@@ -11,7 +11,9 @@ export class UsersListComponent {
   displayedColumns: string[] = ['name', 'date', 'status'];
   usersList: IUser[] = UsersList;
 
-  onUserSelected(user: IUser) {
+  @Output('userSelected') userSelectedEmitt = new EventEmitter<IUser>();
 
+  onUserSelected(user: IUser) {
+    this.userSelectedEmitt.emit(user);
   }
 }
